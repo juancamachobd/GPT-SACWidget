894f38d6feff2a08552a09bee3ee6eed8a33d90f
juancamachobd <33878088+juancamachobd@users.noreply.github.com>
2025-04-24 15:22:29 -0400

    Update ChatGptWidget.js
    N:1 number of responses and two console logs for debug

--- ChatGptWidget.js
+++ ChatGptWidget.js
@@ -99,11 +99,14 @@
                { role: "user", content: prompt }
             ],
             max_tokens: parseInt(max_tokens),
-            n: 10,
-            temperature: 0.5
+            n: 1,
+            temperature: 0.8
           })
+          console.log('Response:', response);
         });
+        
         const data = await response.json();
+        console.log('API Response:', data);
         if (data.choices && data.choices.length > 0) {
           const generatedTextValue = data.choices[0].message.content;
           const processedText = generatedTextValue.replace(/\n/g, '');
