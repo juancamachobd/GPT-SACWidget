bce84c0007102df6f2722ecbc3029746c2b87934
juancamachobd <33878088+juancamachobd@users.noreply.github.com>
2025-04-24 23:14:01 -0400

    Update ChatGptWidget.js Console

--- ChatGptWidget.js
+++ ChatGptWidget.js
@@ -102,6 +102,7 @@
             n: 1,
             temperature: 0.8
           })
+          console.log('Response:', response);
         });        
         const data = await response.json();
         console.log('API Response:', data);
